e4a867fd77b997b409ad34bedf9e9518b569b3b4
Paweł Sowa <pawel@the-sowa.com>
2023-11-24 20:58:36 +0100

    feat: add basic cube

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,26 @@
-console.log('hello');
+import * as THREE from 'three';
+
+const canvas = document.querySelector('#three-canvas') as HTMLCanvasElement;
+
+const scene = new THREE.Scene();
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const mesh = new THREE.Mesh(geometry, material);
+mesh.position.set(0.6, -0.7, 0.8);
+scene.add(mesh);
+
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.set(0, 0, 3);
+scene.add(camera);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+});
+
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
