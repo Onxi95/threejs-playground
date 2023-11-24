b70f86fa7e92deffe8eb35afef6a2b0f7d2ae500
Paweł Sowa <pawel@the-sowa.com>
2023-11-24 21:13:27 +0100

    feat: add group with 2 cubes

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,25 @@ import * as THREE from 'three';
 const canvas = document.querySelector('#three-canvas') as HTMLCanvasElement;
 
 const scene = new THREE.Scene();
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const mesh = new THREE.Mesh(geometry, material);
-mesh.position.set(0.6, -0.7, 0.8);
-scene.add(mesh);
+
+const group = new THREE.Group();
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+
+cube1.position.set(-1, 0, 0);
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+
+group.add(cube2);
+cube2.position.set(2, 0, 0);
 
 const sizes = {
   width: 800,
