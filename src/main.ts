8e48944d73a53c1968b459602ca462e0d39d5f92
Paweł Sowa <pawel@the-sowa.com>
2023-11-24 21:26:36 +0100

    feat: add group rotation

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ const cube2 = new THREE.Mesh(
 );
 
 group.add(cube2);
-cube2.position.set(2, 0, 0);
+cube2.position.set(1, 0, 0);
 
 const sizes = {
   width: 800,
@@ -32,9 +32,23 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.set(0, 0, 3);
 scene.add(camera);
 
+camera.lookAt(group.position);
+
 const renderer = new THREE.WebGLRenderer({
   canvas,
 });
 
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
+
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const timeDelta = clock.getElapsedTime();
+  group.rotation.y = timeDelta * Math.PI;
+  group.rotation.x = timeDelta * Math.PI;
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+};
+
+tick();
