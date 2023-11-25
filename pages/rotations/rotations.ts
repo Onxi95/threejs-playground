8fd5fec7e7aeed908ab1827a1b15504702daac17
Paweł Sowa <pawel@the-sowa.com>
2023-11-25 13:17:47 +0100

    feat: add rotations

---
+++ pages/rotations/rotations.ts
@@ -0,0 +1,56 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+const canvas = document.querySelector('#three-canvas') as HTMLCanvasElement;
+
+const scene = new THREE.Scene();
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
+);
+cube1.position.set(0, 0, 0);
+scene.add(cube1);
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight
+);
+camera.position.set(0, 0, 3);
+scene.add(camera);
+
+const orbit = new OrbitControls(camera, canvas);
+orbit.enableDamping = true;
+camera.lookAt(cube1.position);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+});
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.render(scene, camera);
+
+const clock = new THREE.Clock();
+
+const cursor = {
+  x: 0,
+  y: 0,
+};
+
+const tick = () => {
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+  orbit.update();
+};
+
+tick();
+
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+window.addEventListener('mousemove', (event) => {
+  cursor.x = event.clientX / window.innerWidth - 0.5;
+  cursor.y = -(event.clientY / window.innerHeight - 0.5);
+});
