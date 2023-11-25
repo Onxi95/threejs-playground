5967edd4b856ca43bcbb04cf3ef3219761596be8
Paweł Sowa <pawel@the-sowa.com>
2023-11-25 12:27:11 +0100

    feat: add animation subfolder

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import config from './package.json';
 
-export default defineConfig({
-  base: config.name,
-});
+export default defineConfig(({ mode }) => ({
+  appType: 'mpa',
+
+  base: mode === 'production' ? `/${config.name}` : '/',
+}));
