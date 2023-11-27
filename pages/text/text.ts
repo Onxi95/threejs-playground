c2211939876affa68b62c736346ce8678d13cfee
Paweł Sowa <pawel@the-sowa.com>
2023-11-27 20:17:33 +0100

    feat: add `Sowel`

--- pages/text/text.ts
+++ pages/text/text.ts
@@ -10,7 +10,7 @@ scene.background = new THREE.Color(0x0f1729);
 
 const fontLoader = new FontLoader();
 fontLoader.load('/helvetiker_bold.typeface.json', (font) => {
-  const textGeometry = new TextGeometry('Soweł', {
+  const textGeometry = new TextGeometry('Sowel', {
     font,
     size: 0.5,
     height: 0.2,
