e396a2bdc853137c3e9f385153ef5fbfa1384c74
Paweł Sowa <pawel@the-sowa.com>
2023-12-02 09:48:21 +0100

    feat: add basic particle element

---
+++ pages/particles/particles.ts
@@ -0,0 +1,59 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+const canvas = document.querySelector('#three-canvas') as HTMLCanvasElement;
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x0f1729);
+
+const particlesGeometry = new THREE.SphereGeometry(1, 64, 64);
+const particlesMaterial = new THREE.PointsMaterial({
+  size: 0.02,
+  color: new THREE.Color(0xffffff),
+});
+const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+scene.add(particles);
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight
+);
+camera.position.set(0, 0, 10);
+scene.add(camera);
+
+const orbit = new OrbitControls(camera, canvas);
+orbit.enableDamping = true;
+
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+});
+renderer.shadowMap.enabled = true;
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.render(scene, camera);
+
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  particles.rotation.y = Math.tan(elapsedTime);
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+  orbit.update();
+};
+
+tick();
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+window.addEventListener('dblclick', () => {
+  if (document.fullscreenElement) {
+    return document.exitFullscreen();
+  }
+  canvas.requestFullscreen();
+});
